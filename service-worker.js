1c250c7a4ca4e658386379e1fae1ce3fbfea12b0
PrismaLab-arm64 <252743246+PrismaLab-arm64@users.noreply.github.com>
2026-03-10 14:27:31 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,42 @@
-const CACHE_NAME="form2go-cache-v6"
+const CACHE_NAME = "form2go-cache-v7";
 
-const ASSETS=[
+const ASSETS = [
+  "/Form2Go/",
+  "/Form2Go/index.html",
+  "/Form2Go/manifest.json",
+  "/Form2Go/css/styles.css",
+  "/Form2Go/js/app.js",
+  "/Form2Go/assets/icons/icon-192.png",
+  "/Form2Go/assets/icons/icon-512.png"
+];
 
-"/",
-"/index.html",
-"/styles.css",
-"/js/app.js",
-"/manifest.json",
-"/assets/icons/icon-192.png",
-"/assets/icons/icon-512.png"
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
 
-]
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return null;
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
 
-self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache=>cache.addAll(ASSETS))
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request)
-.then(res=>res||fetch(e.request))
-
-)
-
-})
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
